1b22db88231d5864854f2121c72325e7df25afef
macari216 <akuchuruk@gmail.com>
2024-05-29 13:04:12 -0400

    Basic visualization

--- data.py
+++ data.py
@@ -9,3 +9,18 @@ off_time = sio.loadmat('c57LightOffTime.mat')['c57LightOffTime']
 
 spikes_all = sio.loadmat('c57SpikeTimesAll.mat')['c57SpikeTimesAll']
 spikes_quiet = sio.loadmat('c57SpikeTimesQuiet.mat')['c57SpikeTimesQuiet']
+
+
+# idx = 100
+#
+# arr = np.array([[1],[2],[3]])
+# spikeslist = spikes_all.tolist()
+# spike_times = spikeslist[idx][0]
+# print(np.shape(spike_times))
+#
+# song = np.arange(song_times[1,0], song_times[1,1], 1e-8)
+# spike_times_plot = spike_times[(spike_times>=song[0]) & (spike_times<song[-1])]
+#
+# fig1, ax1 = plt.subplots(1)
+# ax1.plot(spike_times_plot, [1]*spike_times_plot.size, 'ko')
+# plt.show()
