35429728120875bcec293b37dd989c762f2ac440
macari216 <akuchuruk@gmail.com>
2024-06-19 16:41:00 -0400

    added minibatching

--- songbird_GLM_quiet.py
+++ songbird_GLM_quiet.py
@@ -5,19 +5,21 @@ import pynapple as nap
 import nemos as nmo
 from sklearn.utils import gen_batches
 
-song_times = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds/c57SongTimes.mat')['c57SongTimes']
 audio_segm = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds/c57AudioSegments.mat')['c57AudioSegments']
 off_time = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds/c57LightOffTime.mat')['c57LightOffTime']
 spikes_quiet = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds/c57SpikeTimesQuiet.mat')['c57SpikeTimesQuiet']
-spikes_all = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds/c57SpikeTimesAll.mat')['c57SpikeTimesAll']
 
 #convert times to Interval Sets and spikes to TsGroups
 audio_segm = nap.IntervalSet(start=audio_segm[:,0], end=audio_segm[:,1])
 
 audio_on = audio_segm[:149]
-time_on = nap.IntervalSet(0, off_time)
-time_quiet = time_on.set_diff(audio_on)
-time_quiet = time_quiet.drop_short_intervals(1,'s')
+training_end = off_time * 0.7
+time_on_train = nap.IntervalSet(0, training_end)
+time_on_test = nap.IntervalSet(training_end, off_time)
+time_quiet_train = time_on_train.set_diff(audio_on)
+time_quiet_train = time_quiet_train.drop_short_intervals(1,'s')
+time_quiet_test = time_on_test.set_diff(audio_on)
+time_quiet_test = time_quiet_test.drop_short_intervals(1,'s')
 
 ts_dict_quiet = {key: nap.Ts(spikes_quiet[key, 0].flatten()) for key in range(spikes_quiet.shape[0])}
 spike_times_quiet = nap.TsGroup(ts_dict_quiet)
@@ -26,42 +28,64 @@ spike_times_quiet = nap.TsGroup(ts_dict_quiet)
 spike_times_quiet["neuron_subset"] = [0] * 195 + [1] * 0
 
 # spike count
-binsize = 0.1   # in seconds
-# count = spike_times_quiet[spike_times_quiet.neuron_subset == 0].count(binsize, ep=time_quiet)
-count = spike_times_quiet.count(binsize, ep=time_quiet)
+binsize = 0.005   # in seconds
+count_train = spike_times_quiet.count(binsize, ep=time_quiet_train)
+count_test = spike_times_quiet.count(binsize, ep=time_quiet_test)
 
 #choose spike history window
-hist_window_sec = 0.9
+hist_window_sec = 0.05
 hist_window_size = int(hist_window_sec * count.rate)
+
+n_bat = 1000
+batch_size = count_train.time_support.tot_length() / n_bat
+
+def batcher(start):
+    end = start + batch_size
+    ep = nap.IntervalSet(start, end)
+    start = end
+    counts = count_train.restrict(ep)
+    X = basis.compute_features(counts)
+
+    return X, counts, start
+
+logl = np.zeros((len(hist_window_size), n_bat))
 
 # define  basis
 basis = nmo.basis.RaisedCosineBasisLog(9, mode="conv", window_size=hist_window_size)
 time, basis_kernels = basis.evaluate_on_grid(hist_window_size)
 time *= hist_window_sec
 
-X = basis.compute_features(count)
+X_test = basis.compute_features(count_test)
 
-# train test split 70-30
-duration = count.time_support.tot_length("s")
-start = count.time_support["start"]
-end = count.time_support["end"]
-training = nap.IntervalSet(start[0], start[0] + duration * 0.7)
-testing = nap.IntervalSet(start[0] + duration * 0.7, end[-1])
-
-# define and train model
+# define and initialize model
 model = nmo.glm.PopulationGLM(regularizer=nmo.regularizer.Ridge(regularizer_strength=1.0, solver_name="LBFGS"))
-model.fit(X.restrict(training), count.restrict(training).squeeze())
+start = count_train.time_support.start[0]
+params, state = model.initialize_solver(*batcher(start))
+
+score_train = np.zeros(n_bat)
+score_test = np.zeros(n_bat)
+
+# train model
+for i in range(n_bat):
+    # Get a batch of data
+    X, Y, start = batcher()
+
+    # Do one step of gradient descent.
+    params, state = model.update(params, state, X, Y)
+
+    # Score the model along the time axis
+    score_train[i] = model.score(X, Y, score_type="pseudo-r2-McFadden")
+    score_test[i] = model.score(X_test, count_test.squeeze(), score_type="pseudo-r2-McFadden")
+
 
 # compute score
-score_train = model.score(X.restrict(training), count.restrict(training).squeeze(), score_type="pseudo-r2-McFadden")
-score_test = model.score(X.restrict(testing), count.restrict(testing).squeeze(), score_type="pseudo-r2-McFadden")
-print("Score(train data):", score_train)
-print("Score(test data):", score_test)
+print("Score(train data):", score_train[-1])
+print("Score(test data):", score_test[-1])
 
 # model output
-weights = model.coef_.reshape(count.shape[1], basis.n_basis_funcs, count.shape[1])
+weights = model.coef_.reshape(count_train.shape[1], basis.n_basis_funcs, count_train.shape[1])
 filters = np.einsum("jki,tk->ijt", weights, basis_kernels)
-spike_pred = model.predict(X.restrict(testing))
+spike_pred = model.predict(X_test)
 intercept = model.intercept_
 coef = model.coef_
 results_dict = {"weights": weights, "filters": filters, "intercept": intercept, "coef": coef,
