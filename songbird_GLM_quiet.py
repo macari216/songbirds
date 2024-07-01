1898716b8a070b1da92d8f815e739312737f14bc
macari216 <akuchuruk@gmail.com>
2024-07-01 12:52:29 -0400

    minibatching with 5/30

--- songbird_GLM_quiet.py
+++ songbird_GLM_quiet.py
@@ -3,39 +3,57 @@ import scipy.io as sio
 import matplotlib.pyplot as plt
 import pynapple as nap
 import nemos as nmo
-from sklearn.utils import gen_batches
+import pandas as pd
+
+nap.nap_config.suppress_conversion_warnings = True
 
 audio_segm = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds/c57AudioSegments.mat')['c57AudioSegments']
 off_time = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds/c57LightOffTime.mat')['c57LightOffTime']
 spikes_quiet = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds/c57SpikeTimesQuiet.mat')['c57SpikeTimesQuiet']
+ei_labels = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds/c57EI.mat')['c57EI']
 
 #convert times to Interval Sets and spikes to TsGroups
 audio_segm = nap.IntervalSet(start=audio_segm[:,0], end=audio_segm[:,1])
 
-audio_on = audio_segm[:149]
-training_end = off_time * 0.7
-time_on_train = nap.IntervalSet(0, training_end)
-time_on_test = nap.IntervalSet(training_end, off_time)
-time_quiet_train = time_on_train.set_diff(audio_on)
-time_quiet_train = time_quiet_train.drop_short_intervals(1,'s')
-time_quiet_test = time_on_test.set_diff(audio_on)
-time_quiet_test = time_quiet_test.drop_short_intervals(1,'s')
-
 ts_dict_quiet = {key: nap.Ts(spikes_quiet[key, 0].flatten()) for key in range(spikes_quiet.shape[0])}
 spike_times_quiet = nap.TsGroup(ts_dict_quiet)
 
-# add neuron subset marker
-spike_times_quiet["neuron_subset"] = [0] * 195 + [1] * 0
+# add E/I labels
+spike_times_quiet["EI"] = ei_labels
+
+inh = spike_times_quiet.getby_category("EI")[-1]
+exc = spike_times_quiet.getby_category("EI")[1]
 
 # spike count
-binsize = 0.005   # in seconds
-count_train = spike_times_quiet.count(binsize, ep=time_quiet_train)
-count_test = spike_times_quiet.count(binsize, ep=time_quiet_test)
+training_end = off_time * 0.7
+time_quiet_train = nap.IntervalSet(0, training_end).set_diff(audio_segm)
+time_quiet_train = time_quiet_train.drop_short_intervals(1,'s')
+time_quiet_test = nap.IntervalSet(training_end, off_time).set_diff(audio_segm)
+time_quiet_test = time_quiet_test.drop_short_intervals(1,'s')
+
+binsize = 0.001   # in seconds
+count_train = nap.TsdFrame(pd.concat([exc.count(binsize, ep=time_quiet_train).as_dataframe(),
+                        inh.count(binsize, ep=time_quiet_train).as_dataframe()], axis=1))
+
+count_test = nap.TsdFrame(pd.concat([exc.count(binsize, ep=time_quiet_test).as_dataframe(),
+                        inh.count(binsize, ep=time_quiet_test).as_dataframe()], axis=1))
+
+n_neurons = count_train.shape[1]
 
 #choose spike history window
-hist_window_sec = 0.05
-hist_window_size = int(hist_window_sec * count.rate)
+hist_window_sec = 0.01
+hist_window_size = int(hist_window_sec * count_train.rate)
 
+# define  basis
+n_fun = 5
+basis = nmo.basis.RaisedCosineBasisLog(n_fun, mode="conv", window_size=hist_window_size)
+time, basis_kernels = basis.evaluate_on_grid(hist_window_size)
+time *= hist_window_sec
+
+X_test = basis.compute_features(count_test)
+
+
+# implement minibatching
 n_bat = 1000
 batch_size = count_train.time_support.tot_length() / n_bat
 
@@ -48,42 +66,47 @@ def batcher(start):
 
     return X, counts, start
 
-logl = np.zeros((len(hist_window_size), n_bat))
+# mask for group lasso
+n_groups = n_neurons
+n_features = n_neurons * n_fun
 
-# define  basis
-basis = nmo.basis.RaisedCosineBasisLog(9, mode="conv", window_size=hist_window_size)
-time, basis_kernels = basis.evaluate_on_grid(hist_window_size)
-time *= hist_window_sec
-
-X_test = basis.compute_features(count_test)
+mask = np.zeros((n_groups, n_features))
+for i in range(n_groups):
+    mask[i, i*n_fun:i*n_fun+n_fun] = np.ones(n_fun)
 
 # define and initialize model
-model = nmo.glm.PopulationGLM(regularizer=nmo.regularizer.Ridge(regularizer_strength=1.0, solver_name="LBFGS"))
+model = nmo.glm.PopulationGLM(regularizer=nmo.regularizer.GroupLasso(
+    solver_name="ProximalGradient", mask=mask, solver_kwargs={"stepsize": 0.1, "acceleration": False},
+    regularizer_strength=2e-5))
 start = count_train.time_support.start[0]
 params, state = model.initialize_solver(*batcher(start))
 
-score_train = np.zeros(n_bat)
-score_test = np.zeros(n_bat)
-
 # train model
-for i in range(n_bat):
-    # Get a batch of data
-    X, Y, start = batcher()
+n_ep = 200
+score_train = np.zeros(n_ep)
 
-    # Do one step of gradient descent.
-    params, state = model.update(params, state, X, Y)
+for ep in range(n_ep):
+    start = count_train.time_support.start[0]
+    for i in range(n_bat):
+        # Get a batch of data
+        X, Y, start = batcher(start)
+
+        # Do one step of gradient descent.
+        params, state = model.update(params, state, X, Y)
 
     # Score the model along the time axis
-    score_train[i] = model.score(X, Y, score_type="pseudo-r2-McFadden")
-    score_test[i] = model.score(X_test, count_test.squeeze(), score_type="pseudo-r2-McFadden")
+    score_train[ep] = model.score(X, Y, score_type="log-likelihood")
 
+    if ep%5 ==0:
+        print("Epoch:", ep, ", score:", score_train[ep])
 
 # compute score
+score_test = model.score(X_test, count_test.squeeze(), score_type="log-likelihood")
 print("Score(train data):", score_train[-1])
-print("Score(test data):", score_test[-1])
+print("Score(test data):", score_test)
 
 # model output
-weights = model.coef_.reshape(count_train.shape[1], basis.n_basis_funcs, count_train.shape[1])
+weights = model.coef_.reshape(n_neurons, basis.n_basis_funcs, n_neurons)
 filters = np.einsum("jki,tk->ijt", weights, basis_kernels)
 spike_pred = model.predict(X_test)
 intercept = model.intercept_
@@ -91,4 +114,10 @@ coef = model.coef_
 results_dict = {"weights": weights, "filters": filters, "intercept": intercept, "coef": coef,
                 "spike_pred": spike_pred, "time": time}
 
-np.save("results.npy", results_dict)
+np.save("results_gl_sorted.npy", results_dict)
+
+plt.figure()
+plt.plot(score_train)
+plt.xlabel("Epoch")
+plt.ylabel("log-likelihood")
+plt.show()
