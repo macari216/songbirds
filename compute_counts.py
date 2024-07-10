c264a6a2ea300ab54da204d5407efadd46272e39
macari216 <akuchuruk@gmail.com>
2024-07-10 11:55:11 -0400

    added args

--- compute_counts.py
+++ compute_counts.py
@@ -1,6 +1,11 @@
 import scipy.io as sio
 import pynapple as nap
 from datetime import datetime
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-n", "--Neuron", help="Specify GLM input neuron (0-194)")
+args = parser.parse_args()
 
 print(f"before loading: {datetime.now().time()}")
 audio_segm = sio.loadmat('/mnt/home/amedvedeva/ceph/songbird_data/c57AudioSegments.mat')['c57AudioSegments']
@@ -31,7 +36,7 @@ def batcher(start):
     print(f"before computing batches counts: {datetime.now().time()}")
     X_counts = spike_times_quiet.count(binsize, ep=ep)
     print(f"after computing batched X counts: {datetime.now().time()}")
-    Y_counts = spike_times_quiet[0].count(binsize, ep=ep)
+    Y_counts = spike_times_quiet[int(args.Neuron)].count(binsize, ep=ep)
     print(f"after computing batched Y counts: {datetime.now().time()}")
     return X_counts, Y_counts, start
 
@@ -45,3 +50,4 @@ print(f"before computing counts: {datetime.now().time()}")
 counts = spike_times_quiet.count(binsize, ep=time_on)
 print(f"after computing counts: {datetime.now().time()}")
 
+c
