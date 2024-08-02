fdd040a0dcdc5603c85e4b9722f34af97e3f0071
macari216 <akuchuruk@gmail.com>
2024-08-02 16:43:47 -0400

    added plot for neuron types

--- model_plots.py
+++ model_plots.py
@@ -3,20 +3,67 @@ import scipy.io as sio
 import pynapple as nap
 import nemos as nmo
 import matplotlib.pyplot as plt
+from matplotlib import colors
 import pandas as pd
 from itertools import combinations, product
 import seaborn as sns
 
 results_dict = np.load("/songbirds/results.npy", allow_pickle=True).item()
-weights = results_dict["weights"]
-filters = results_dict["filters"]
-time = results_dict["time"]
-spike_pred = results_dict["spike_pred"]
+coef = results_dict["params"][0]
+bias = results_dict["params"][1]
+train_ll = np.array(results_dict["train_ll"])
+test_ll = train_ll[-1]
+train_ll = train_ll[:-1]
+
+audio_segm = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds_data/c57AudioSegments.mat')['c57AudioSegments']
+off_time = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds_data/c57LightOffTime.mat')['c57LightOffTime']
+spikes_quiet = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds_data/c57SpikeTimesQuiet.mat')['c57SpikeTimesQuiet']
+ei_labels = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds_data/c57EI.mat')['c57EI']
+
+ts_dict_quiet = {key: nap.Ts(spikes_quiet[key, 0].flatten()) for key in range(spikes_quiet.shape[0])}
+spike_times_quiet = nap.TsGroup(ts_dict_quiet)
+
+n_neurons = 195
+n_fun = 9
+binsize = 0.0001
+hist_window_sec = 0.004
+hist_window_size = int(hist_window_sec / binsize)
+basis = nmo.basis.RaisedCosineBasisLog(n_fun, mode="conv", window_size=hist_window_size)
+time, basis_kernels = basis.evaluate_on_grid(hist_window_size)
+time *= hist_window_sec
+
+weights = coef.reshape(n_neurons, n_fun, n_neurons)
+filters = np.einsum("jki,tk->ijt", weights, basis_kernels)
+
+filters_df = {}
+pairs = product(list(spike_times_quiet.keys()),list(spike_times_quiet.keys()))
+for i,j in pairs:
+    filters_df[(i,j)] = filters[j,i,:]
+filters_df = pd.DataFrame.from_dict(filters_df)
+
+conx_type = {}
+pairs = product(list(spike_times_quiet.keys()),list(spike_times_quiet.keys()))
+for i,j in pairs:
+    conx_type[(i,j)] = (ei_labels[i][0], ei_labels[j][0])
+
+filters_df = filters_df._append(conx_type, ignore_index=True)
+
+time_ind = {}
+for i in range(40):
+    time_ind[i]=np.round(time[i], 6)
+time_ind[40] = 'ei'
+filters_df = filters_df.rename(index=time_ind)
+
+idx = [40] + [i for i in range(len(filters_df)) if i !=40]
+filters_df = filters_df.iloc[idx]
+
+filters_df_sorted = filters_df.sort_values(by='ei', axis=1, ascending=False)
+
+
+
+
 
 song_times = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds/c57SongTimes.mat')['c57SongTimes']
-audio_segm = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds/c57AudioSegments.mat')['c57AudioSegments']
-off_time = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds/c57LightOffTime.mat')['c57LightOffTime']
-spikes_quiet = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds/c57SpikeTimesQuiet.mat')['c57SpikeTimesQuiet']
 spikes_all = sio.loadmat('/Users/macari216/Desktop/glm_songbirds/songbirds/c57SpikeTimesAll.mat')['c57SpikeTimesAll']
 
 song_times = nap.IntervalSet(start=song_times[:,0], end=song_times[:,1])
@@ -34,8 +81,6 @@ time_quiet_test = time_quiet_test.drop_short_intervals(1,'s')
 ts_dict_all = {key: nap.Ts(spikes_all[key, 0].flatten()) for key in range(spikes_all.shape[0])}
 spike_times_all = nap.TsGroup(ts_dict_all)
 
-ts_dict_quiet = {key: nap.Ts(spikes_quiet[key, 0].flatten()) for key in range(spikes_quiet.shape[0])}
-spike_times_quiet = nap.TsGroup(ts_dict_quiet)
 
 binsize = 0.005
 count_test = spike_times_quiet.count(binsize, ep=time_quiet_test)
@@ -58,31 +103,31 @@ plt.xlabel("Time from spike (sec)")
 plt.ylabel("Weight")
 
 # plot spiking rates
-ep = nap.IntervalSet(8716, 8716+60)
-
-fig = plt.figure(figsize=(8,4))
-ax1 = fig.add_subplot(2,1,1)
-firing_rate = spike_pred.restrict(ep).d[:,:15]
-firing_rate = firing_rate.T / np.max(firing_rate, axis=1)
-plt.imshow(firing_rate[::-1], cmap="Greys", aspect="auto")
-ax1.set_ylabel("Neuron")
-ax1.set_title("Predicted firing rate")
-ax2 = fig.add_subplot(2,1,2)
-firing_rate = count_test.restrict(ep).d[:,:15]
-firing_rate = firing_rate.T / np.max(firing_rate, axis=1)
-plt.imshow(firing_rate[:,:-1], cmap="Greys", aspect="auto")
-ax2.set_ylabel("Neuron")
-ax2.set_title("True firing rate")
-fig.subplots_adjust(hspace=1)
-plt.xlabel("Time (sec)")
-fig.suptitle("Resting Data")
+# ep = nap.IntervalSet(8716, 8716+60)
+#
+# fig = plt.figure(figsize=(8,4))
+# ax1 = fig.add_subplot(2,1,1)
+# firing_rate = spike_pred.restrict(ep).d[:,:15]
+# firing_rate = firing_rate.T / np.max(firing_rate, axis=1)
+# plt.imshow(firing_rate[::-1], cmap="Greys", aspect="auto")
+# ax1.set_ylabel("Neuron")
+# ax1.set_title("Predicted firing rate")
+# ax2 = fig.add_subplot(2,1,2)
+# firing_rate = count_test.restrict(ep).d[:,:15]
+# firing_rate = firing_rate.T / np.max(firing_rate, axis=1)
+# plt.imshow(firing_rate[:,:-1], cmap="Greys", aspect="auto")
+# ax2.set_ylabel("Neuron")
+# ax2.set_title("True firing rate")
+# fig.subplots_adjust(hspace=1)
+# plt.xlabel("Time (sec)")
+# fig.suptitle("Resting Data")
 
 
 # plot history filters
 filter_plot = filters[:20,:20,:]
 
 def plot_coupling(responses, cmap_name="bwr",
-                      figsize=(8, 6), fontsize=15, alpha=0.5, cmap_label="hsv"):
+                      figsize=(8, 6), fontsize=15, alpha=0.5):
 
     # plot heatmap
     sum_resp = np.sum(responses, axis=2)
@@ -96,7 +141,7 @@ def plot_coupling(responses, cmap_name="bwr",
     cmap = plt.get_cmap(cmap_name)
     n_row, n_col, n_tp = responses.shape
     time = np.arange(n_tp)
-    fig, axs = plt.subplots(n_row, n_col, figsize=figsize, sharey="row")
+    fig, axs = plt.subplots(n_row, n_col, figsize=figsize, sharey=True)
     for rec, rec_resp in enumerate(responses):
         for send, resp in enumerate(rec_resp):
             axs[rec, send].plot(time, responses[rec, send], color="k")
@@ -133,6 +178,17 @@ sum_filt = np.sum(filters, axis=2)
 sum_filt_n = (sum_filt.T / np.abs(sum_filt).max(axis=1)).T
 plt.figure()
 plt.imshow(sum_filt_n, cmap='bwr')
+plt.colorbar()
+plt.xlabel("pre-synaptic")
+plt.ylabel("post-synaptic")
+plt.title("inferred filters (group lasso)")
+
+cmap = colors.ListedColormap(['white', 'black'])
+bounds=[0,1e-15, 1e-2]
+norm = colors.BoundaryNorm(bounds, cmap.N)
+plt.figure()
+plt.imshow(sum_filt, interpolation='nearest', origin='lower',
+                    cmap=cmap, norm=norm)
 plt.colorbar()
 plt.xlabel("pre-synaptic")
 plt.ylabel("post-synaptic")
@@ -210,13 +266,17 @@ ax2.set_xlabel('time from spike (sec)')
 # plt.hist(diff, bins=50)
 # diff[np.logical_and(diff > -0.007, diff < 0.0023)].size
 #
-# amplitudes_fh = np.zeros(count.shape[1])
-# for i in range(count.shape[1]):
-#     amplitudes_fh[i] = np.max(weights_fh[i, :, i]) - np.min(weights_fh[i, :, i])
+# amplitudes_fh = {}
+# amplitudes_sh = {}
+# amplitudes_fh = pd.DataFrame.from_dict(amplitudes_fh, orient="index")
+# amplitudes_sh = pd.DataFrame.from_dict(amplitudes_sh, orient="index")
 #
-# amplitudes_sh = np.zeros(count.shape[1])
-# for i in range(count.shape[1]):
-#     amplitudes_sh[i] = np.max(weights_sh[i, :, i]) - np.min(weights_sh[i, :, i])
+# pairs = product(list(spike_times_quiet.keys()),list(spike_times_quiet.keys()))
+#
+#
+# for i,j in pairs:
+#     amplitudes_fh[(i,j)]=np.max(weights_fh[i, :, j]) - np.min(weights_fh[i, :, j])
+#     amplitudes_sh[(i, j)] = np.max(weights_sh[i, :, j]) - np.min(weights_sh[i, :, j])
 #
 # plt.figure()
 # plt.scatter(amplitudes_fh, amplitudes_sh, color='k')
@@ -317,6 +377,31 @@ def ccg_filt_plot(n1, n2, counts, filters):
     fig.subplots_adjust(wspace=0.3)
     return fig
 
+def ccg_filt_plot_eb(n1, n2, counts, filters):
+    filters_mean = filters.mean(0)[n2,n1]
+    filters_lower = filters_mean - filters.std(0)[n2,n1]
+    filters_upper = filters_mean + filters.std(0)[n2,n1]
+
+    x_ticks = np.round(counts[(n1, n2)].index, 3)
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
+    ax1.bar(counts[(n1, n2)].index, counts[(n1, n2)], width=0.0001)
+    ax1.axvline(0, 0, 1, color='r', ls='--')
+    ax1.set_xticks(x_ticks, labels=(x_ticks * 1000).astype(int))
+    ax1.set_xlabel("lag (ms)")
+    ax1.set_ylabel("spike count")
+    ax1.set_title(f"CCG between {n1} (ref) and {n2} (target)")
+    ax2.plot(time, filters_mean, label='filter mean')
+    ax2.plot(time, filters_lower, color='tab:blue', alpha=0.1)
+    ax2.plot(time, filters_upper, color='tab:blue', alpha=0.1)
+    ax2.fill_between(time, filters_lower, filters_upper, alpha=0.2)
+    ax2.set_xlabel("lag (ms)")
+    ax2.set_ylabel("gain")
+    x_ticks = x_ticks[x_ticks >= 0]
+    ax2.set_xticks(x_ticks, labels=(x_ticks * 1000).astype(int))
+    ax2.set_title(f"Filter from {n1} to {n2}")
+    fig.subplots_adjust(wspace=0.3)
+    return fig
+
 # ACG
 def comp_acg(n1):
     n1_spikes = {n1: nap.Ts(spikes_quiet[n1, 0].flatten())}
@@ -331,7 +416,7 @@ def comp_acg(n1):
 
 def acg_filt_plot(n1, counts, filters):
     x_ticks = np.array([-0.15,0.15])
-    filter = filters[:,n1]
+    filter = filters[n1,n1]
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
     ax1.bar(counts[n1].index, counts[n1], width=0.0001)
     ax1.axvline(0, 0, 1, color='r', ls='--')
@@ -350,9 +435,7 @@ def acg_filt_plot(n1, counts, filters):
 
 
 ccg = nap.compute_crosscorrelogram((spike_times_quiet,spike_times_quiet), 0.0001, 0.004, norm=False)
-acg = nap.compute_autocorrelogram(spike_times_quiet, 0.0001, 0.15, norm=False)
-
-pairs = product(list(spike_times_quiet),list(spike_times_quiet))
+pairs = product(list(spike_times_quiet.keys()),list(spike_times_quiet.keys()))
 ccg_counts = {}
 for i,j in pairs:
     t1 = spike_times_quiet[i].index
@@ -365,6 +448,7 @@ for i in range(195):
 
 frates_sorted = ccg.max(0).sort_values(ascending=False)
 
+acg = nap.compute_autocorrelogram(spike_times_quiet, 0.0001, 0.15, norm=False)
 acg_counts ={}
 for i in range(195):
     t1 = spike_times_quiet[i].index
@@ -380,3 +464,56 @@ plt.bar(ccg_counts[(124, 125)].index, ccg_counts[(124, 125)], width=0.0001)
 plt.vlines(0, 0, max(ccg_counts[(124, 125)]), color='r', ls='--')
 sns.kdeplot(x=ccg_counts[(124,125)].index, weights=ccg_counts[(124,125)].values, color='yellow', bw_adjust=0.03, cut=0)
 
+for i in range(195):
+    ccg = ccg.drop(labels=(i,i), axis=1)
+
+fr_sorted = ccg.max(0).sort_values(ascending=False)
+
+spike_times = nap.TsGroup(ts_dict_quiet)
+spike_times["EI"] = ei_labels
+inh = spike_times.getby_category("EI")[-1]
+exc = spike_times.getby_category("EI")[1]
+spike_times_sorted = nap.TsGroup.merge(exc, inh, reset_index=True)
+
+ccg_sorted = nap.compute_crosscorrelogram((spike_times_sorted,spike_times_sorted), 0.0001, 0.004, norm=False)
+pairs = product(list(spike_times.keys()),list(spike_times.keys()))
+ccg_s_counts = {}
+for i,j in pairs:
+    t1 = spike_times[i].index
+    nt1 = len(t1)
+    ccg_s_counts[(i,j)] = ccg_sorted[(i,j)] * (nt1*0.0001)
+ccg_s_counts = pd.DataFrame.from_dict(ccg_s_counts)
+
+filter_ee = filters[:101,:101]
+filter_ei = filters[-94:,:101]
+filter_ii = filters[-94:,-94:]
+filter_ie = filters[:101,-94:]
+
+ee_nz = filter_ee[filter_ee.sum(2)!=0.0]
+ei_nz = filter_ei[filter_ei.sum(2)!=0.0]
+ii_nz = filter_ii[filter_ii.sum(2)!=0.0]
+ie_nz = filter_ii[filter_ie.sum(2)!=0.0]
+
+fig,axs = plt.subplots(1,7,figsize=(15,3), sharex=True)
+for i in range(7):
+    axs[i].plot(time,ee_nz[i], 'k', lw=2)
+    axs[i].axhline(0, color="k", lw=0.5)
+    axs[i].set(xlabel="lag (s)", ylabel="gain")
+    axs[i].label_outer()
+fig.suptitle("E/E filters")
+
+ee_peaks = np.abs(ee_nz).argmax(axis=1)
+ei_peaks = np.abs(ei_nz).argmax(axis=1)
+ii_peaks = np.abs(ii_nz).argmax(axis=1)
+ie_peaks = np.abs(ie_nz).argmax(axis=1)
+
+plt.figure()
+plt.hist(ee_peaks, bins=7, label="E/E", density=True, alpha=0.5)
+plt.hist(ie_peaks, bins=20, label="I/E", density=True, alpha=0.5)
+plt.hist(ei_peaks, bins=20, label="E/I", density=True, alpha=0.5)
+plt.hist(ii_peaks, bins=20, label="I/I", density=True, alpha=0.5)
+plt.legend()
+plt.xticks(np.arange(0,22, 2), labels=np.arange(0.0,2.2, 0.2))
+plt.xlabel("lag, (ms)")
+plt.ylabel("normalized count")
+plt.title("Peak (dip) delays")
